61cc7e20faad2ad196c844ab12b614c48a3deb1f
aayashahmad <bhatashu666@gmail.com>
2023-05-17 13:33:12 +0530

    addition program

---
+++ src/addition.java
@@ -0,0 +1,11 @@
+public class addition{
+public static void main(String[] args) {
+     
+    int num1 = 5, num2 = 1, sum;
+    sum = num1 + num2;
+ 
+    System.out.println("The sum of two numbers is: "+sum);
+ }
+}
+
+
